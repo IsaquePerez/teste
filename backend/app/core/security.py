6ee4841923b0beab434f6e235f2ea60c82cdcac5
IsaquePerez <isaquenovo127@gmail.com>
2025-12-29 16:39:16 -0300

    Alterando validação de login!

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Any, Union
+from typing import Optional, Any, Union
 from jose import jwt
 from passlib.context import CryptContext
 from app.core.config import settings
@@ -7,17 +7,23 @@ from app.core.config import settings
 # Configura o algoritmo bcrypt para realizar o hashing seguro das senhas.
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Gera um token JWT assinado contendo o ID do usuário e o tempo de expiração.
-def create_access_token(subject: Union[str, Any], expires_delta: timedelta = None) -> str:
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
-    
-    to_encode = {"exp": expire, "sub": str(subject)}
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-
-# Verifica se a senha em texto plano corresponde ao hash armazenado.
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-# Gera o hash criptografado da senha para ser armazenado no banco de dados.
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    # Atualiza o dicionário com a data de expiração
+    to_encode.update({"exp": expire})
+    
+    # Gera o token JWT com todos os dados (sub, role, email, etc)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
